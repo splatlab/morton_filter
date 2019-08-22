cd54cfb441e5bac95b4b49ec333d1c237a336688
Rob Johnson <robj@vmware.com>
2019-08-21 22:07:29 -0700

    tweak benchmark

--- benchmarking/main.cc
+++ benchmarking/main.cc
@@ -20,19 +20,20 @@
 
 using namespace CompressedCuckoo;
 
+uint64_t tv2usec(struct timeval *tv) {
+  return 1000000 * tv->tv_sec + tv->tv_usec;
+}
+
 /* Print elapsed time using the start and end timeval */
 void print_time_elapsed(const char* desc, struct timeval* start, struct
-												timeval* end)
+												timeval* end, uint64_t ops, const char *opname)
 {
-	struct timeval elapsed;
-	if (start->tv_usec > end->tv_usec) {
-		end->tv_usec += 1000000;
-		end->tv_sec--;
-	}
-	elapsed.tv_usec = end->tv_usec - start->tv_usec;
-	elapsed.tv_sec = end->tv_sec - start->tv_sec;
-	float time_elapsed = (elapsed.tv_sec * 1000000 + elapsed.tv_usec)/1000000.f;
-	printf("%s Total Time Elapsed: %f seconds", desc, time_elapsed);
+  uint64_t elapsed_usecs = tv2usec(end) - tv2usec(start);
+	printf("%s Total Time Elapsed: %f seconds", desc, 1.0*elapsed_usecs / 1000000);
+  if (ops) {
+    printf(" (%f nanoseconds/%s)", 1000.0 * elapsed_usecs / ops, opname);
+  }
+  printf("\n");
 }
 
 int main(int argc, char **argv)
@@ -45,6 +46,7 @@ int main(int argc, char **argv)
 	uint64_t nslots = (1ULL << qbits);
 	uint64_t nvals = 85*nslots/100;
 	uint64_t *vals;
+	uint64_t *other_vals;
 
 	/* initialize ququ filter */
 	Morton3_8 filter(nslots);
@@ -52,11 +54,14 @@ int main(int argc, char **argv)
 	/* Generate random values */
 	vals = (uint64_t*)malloc(nvals*sizeof(vals[0]));
 	RAND_bytes((unsigned char *)vals, sizeof(*vals) * nvals);
+	other_vals = (uint64_t*)malloc(nvals*sizeof(other_vals[0]));
+	RAND_bytes((unsigned char *)other_vals, sizeof(*other_vals) * nvals);
+  //memset(vals, 0, nvals*sizeof(vals[0]));
 	srand(0);
-	for (uint64_t i = 0; i < nvals; i++) {
-		//vals[i] = rand() % filter.metadata->range;
-		vals[i] = (1 * vals[i]) % ((1ULL << (qbits + 8)) - 1);
-	}
+	// for (uint64_t i = 0; i < nvals; i++) {
+	// 	//vals[i] = rand() % filter.metadata->range;
+	// 	vals[i] = (1 * vals[i]) % ((1ULL << (qbits + 8)) - 1);
+	// }
 
 	struct timeval start, end;
 	struct timezone tzp;
@@ -70,7 +75,7 @@ int main(int argc, char **argv)
 		}
 	}
 	gettimeofday(&end, &tzp);
-	print_time_elapsed("Insertion time", &start, &end);
+	print_time_elapsed("Insertion time", &start, &end, nvals, "insert");
 	puts("");
 	gettimeofday(&start, &tzp);
 	for (uint64_t i = 0; i < nvals; i++) {
@@ -80,7 +85,21 @@ int main(int argc, char **argv)
 		}
 	}
 	gettimeofday(&end, &tzp);
-	print_time_elapsed("Lookup time", &start, &end);
+	print_time_elapsed("Lookup time", &start, &end, nvals, "successful lookup");
+	gettimeofday(&start, &tzp);
+  uint64_t nfps = 0;
+	/* Lookup hashes in the ququ filter */
+	for (uint64_t i = 0; i < nvals; i++) {
+		if (filter.likely_contains(other_vals[i])) {
+      nfps++;
+		}
+	}
+	gettimeofday(&end, &tzp);
+	print_time_elapsed("Random lookup:", &start, &end, nvals, "random lookup");
+  printf("%lu/%lu positives\n"
+         "FP rate: 1/%f\n",
+         nfps, nvals,
+         1.0 * nvals / nfps);
 
 	return 0;
 }
